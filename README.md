6ab46aa39f91274083b7e3f519e467a4f577a1a7
Alexander Ljungberg <aljungberg@slevenbits.com>
2018-07-31 12:25:47 +0100

    New: Python 3 compatibility.

--- README.md
+++ README.md
@@ -1,90 +1,104 @@
 Hexahexacontadecimal
 ====================
 
-**Hexahexacontadecimal is the most compact way to encode a number into a URL.**
+**The most compact way to encode a number into a URL.**
 
 Hexahexacontadecimal is a compact format to express a number or binary data in a URL. It uses all characters allowed in
-a URL without escaping -- the [unreserved characters](http://tools.ietf.org/html/rfc3986#section-2.3) -- making it the
-shortest possible way to express a positive integer in a URL.
+a URL -- the [unreserved characters](http://tools.ietf.org/html/rfc3986#section-2.3) -- making it the most concise
+way to express a positive integer in a URL.
 
 Note that `urllib.quote` [escapes the tilde character (~)](http://bugs.python.org/issue16285), which is not necessary as
-of RFC3986. The `hhc_url_quote` function is provided to help with this.
+of RFC3986, so if you use this on HHC data you'll waste bytes. Use the provided `hhc_url_quote` function instead if you
+must. By definition though HHC values don't need any URL quoting.
 
 ## Usage
 
-    from hexahexacontadecimal import hhc, hhc_to_int
-
-    print hhc(302231454903657293676544)  # 'iFsGUkO.0tsxw'
-    print hhc_to_int('iFsGUkO.0tsxw')    # 302231454903657293676544L
+    >>> from hexahexacontadecimal import hhc, hhc_to_int
+    >>> print(hhc(302231454903657293676544))
+    iFsGUkO.0tsxw
+    >>> print(hhc_to_int('iFsGUkO.0tsxw'))
+    302231454903657293676544
 
 ### Hexahexacontadecimal vs Base64 in URLs
 
     >>> n = 292231454903657293676544
     >>> import base64
-    >>> hhc_url_quote(base64.urlsafe_b64encode(long_to_binary(n)))
-    'PeHmHzZFTcAAAA%3D%3D'
-    >>> hhc_url_quote(hhc(n))
-    'gpE4Xoy7fw5AO'
+    >>> print(hhc_url_quote(base64.urlsafe_b64encode(long_to_binary(n))))
+    PeHmHzZFTcAAAA%3D%3D
+    >>> print(hhc_url_quote(hhc(n)))
+    gpE4Xoy7fw5AO
 
-Worst case scenario for plain Base64:
+Base64 vs HHC in a bad case for Base64:
 
     >>> n = 64 ** 5 + 1
-    >>> hhc_url_quote(base64.urlsafe_b64encode(long_to_binary(n)))
-    'QAAAAQ%3D%3D'
-    >>> hhc_url_quote(hhc(n))
-    'ucrDZ'
+    >>> print(hhc_url_quote(base64.urlsafe_b64encode(long_to_binary(n))))
+    QAAAAQ%3D%3D
+    >>> print(hhc(n))
+    ucrDZ
 
-Worst case for hexahexacontadecimal:
+Base64 vs HHC in a bad case for HHC:
 
     >>> n = 66 ** 5 + 1
-    >>> hhc_url_quote(base64.urlsafe_b64encode(long_to_binary(n)))
-    'SqUUIQ%3D%3D'
-    >>> hhc_url_quote(hhc(n))
-    '100001'
+    >>> print(hhc_url_quote(base64.urlsafe_b64encode(long_to_binary(n))))
+    SqUUIQ%3D%3D
+    >>> print(hhc(n))
+    100001
 
 That big SHA-512 you always wanted to write in a URL:
 
     >>> n = 2 ** 512
-    >>> hhc_url_quote(base64.urlsafe_b64encode(long_to_binary(n)))
-    'AQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA%3D'
-    >>> hhc_url_quote(hhc(n))
-    'JK84xqGD9FMXPNubPghADlRhBUzlqRscC2h~8xmi99PvuQsUCIB2CHGhMUQR8FLm72.Hbbctkqi89xspay~y4'
+    >>> print(hhc_url_quote(base64.urlsafe_b64encode(long_to_binary(n))))
+    AQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA%3D
+    >>> print(hhc(n))
+    JK84xqGD9FMXPNubPghADlRhBUzlqRscC2h~8xmi99PvuQsUCIB2CHGhMUQR8FLm72.Hbbctkqi89xspay~y4
 
-Massive savings.
+Massive savings!
 
 ### Are the savings really significant?
 
-If you're currently doing your Base64 encoding the naive way, then yes:
+If you're currently doing your Base64 encoding the naive way, then yes. Encoding all numbers up to 100000, HHC will lead
+to much shorter total length of URLs.
 
-    >>> sum(len(hhc_url_quote(base64.urlsafe_b64encode(long_to_binary(n)))) for n in xrange(10 ** 5))
+    >>> sum(len(hhc_url_quote(base64.urlsafe_b64encode(long_to_binary(n)))) for n in range(10 ** 5))
     531584
-    >>> sum(len(hhc_url_quote(hhc(n))) for n in xrange(10 ** 5))
+    >>> sum(len(hhc_url_quote(hhc(n))) for n in range(10 ** 5))
     295578
 
-### But what if I use Base64 without padding?
+#### What if I use Base64 without padding?
 
-Then the savings are not as significant. But it's still an improvement. Using the code from [this StackOverFlow
-question](http://stackoverflow.com/a/561704/76900):
+Then the savings are much less significant. Yet they are still savings. If you're a perfectionist this is the kind of 
+thing you might care about.
+
+Let's test it using the code from [this StackOverFlow question](http://stackoverflow.com/a/561704/76900):
 
     >>> from hexahexacontadecimal.num_encode_base64 import num_encode as num_encode_base64
     >>> n = 64 ** 5 + 1
-    >>> hhc_url_quote(num_encode_base64(n))
-    'BAAAAB'
-    >>> hhc_url_quote(hhc(n))
-    'ucrDZ'
+    >>> print(hhc_url_quote(num_encode_base64(n)))
+    BAAAAB
+    >>> from hexahexacontadecimal.num_encode_base64 import num_decode as num_decode_base64
+    >>> num_decode_base64(hhc_url_quote(num_encode_base64(n))) == n
+    True
+
+    >>> print(hhc(n))
+    ucrDZ
+    >>> hhc_to_int(hhc(n)) == n
+    True
+
     >>> n = 66 ** 5 + 1
-    >>> hhc_url_quote(num_encode_base64(n))
-    'BKpRQh'
-    >>> hhc_url_quote(hhc(n))
-    '100001'
+    >>> print(hhc_url_quote(num_encode_base64(n)))
+    BKpRQh
+    >>> print(hhc_url_quote(hhc(n)))
+    100001
+
     >>> n = 2 ** 512
     >>> hhc_url_quote(num_encode_base64(n))
     'EAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
-    >>> hhc_url_quote(hhc(n))
-    'JK84xqGD9FMXPNubPghADlRhBUzlqRscC2h~8xmi99PvuQsUCIB2CHGhMUQR8FLm72.Hbbctkqi89xspay~y4'
-    >>> sum(len(hhc_url_quote(num_encode_base64(n))) for n in xrange(10 ** 5))
+    >>> print(hhc_url_quote(hhc(n)))
+    JK84xqGD9FMXPNubPghADlRhBUzlqRscC2h~8xmi99PvuQsUCIB2CHGhMUQR8FLm72.Hbbctkqi89xspay~y4
+
+    >>> sum(len(hhc_url_quote(num_encode_base64(n))) for n in range(10 ** 5))
     295840
-    >>> sum(len(hhc_url_quote(hhc(n))) for n in xrange(10 ** 5))
+    >>> sum(len(hhc_url_quote(hhc(n))) for n in range(10 ** 5))
     295578
 
 Why settle for less than perfect?
@@ -108,7 +122,6 @@ HHC is not necessarily the shortest representation of a negative number.
 The sortable variant also supports negative numbers and will yield the natural sort order (small to large),
 like -2, -1, 0, 1, 2.
 
-
 ## Installation
 
     pip install hexahexacontadecimal
@@ -126,6 +139,10 @@ To run the unit tests:
     nosetests --with-doctest
 
 ## Changelog
+
+### 2.2
+
+* Python 3 support (backwards compatible with Python 2.7).
 
 ### 2.1
 
--- hexahexacontadecimal/__init__.py
+++ hexahexacontadecimal/__init__.py
@@ -1,90 +1,104 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
-"""Encode and decode hexahexacontadecimal numbers.
+"""**The most compact way to encode a number into a URL.**
 
 Hexahexacontadecimal is a compact format to express a number or binary data in a URL. It uses all characters allowed in
-a URL without escaping -- the [unreserved characters](http://tools.ietf.org/html/rfc3986#section-2.3) -- making it the
-shortest possible way to express a positive integer in a URL.
+a URL -- the [unreserved characters](http://tools.ietf.org/html/rfc3986#section-2.3) -- making it the most concise
+way to express a positive integer in a URL.
 
 Note that `urllib.quote` [escapes the tilde character (~)](http://bugs.python.org/issue16285), which is not necessary as
-of RFC3986. The `hhc_url_quote` function is provided to help with this.
+of RFC3986, so if you use this on HHC data you'll waste bytes. Use the provided `hhc_url_quote` function instead if you
+must. By definition though HHC values don't need any URL quoting.
 
 ## Usage
 
-    from hexahexacontadecimal import hhc, hhc_to_int
-
-    print hhc(302231454903657293676544)  # 'iFsGUkO.0tsxw'
-    print hhc_to_int('iFsGUkO.0tsxw')    # 302231454903657293676544L
+    >>> from hexahexacontadecimal import hhc, hhc_to_int
+    >>> print(hhc(302231454903657293676544))
+    iFsGUkO.0tsxw
+    >>> print(hhc_to_int('iFsGUkO.0tsxw'))
+    302231454903657293676544
 
 ### Hexahexacontadecimal vs Base64 in URLs
 
     >>> n = 292231454903657293676544
     >>> import base64
-    >>> hhc_url_quote(base64.urlsafe_b64encode(long_to_binary(n)))
-    'PeHmHzZFTcAAAA%3D%3D'
-    >>> hhc_url_quote(hhc(n))
-    'gpE4Xoy7fw5AO'
+    >>> print(hhc_url_quote(base64.urlsafe_b64encode(long_to_binary(n))))
+    PeHmHzZFTcAAAA%3D%3D
+    >>> print(hhc_url_quote(hhc(n)))
+    gpE4Xoy7fw5AO
 
-Worst case scenario for plain Base64:
+Base64 vs HHC in a bad case for Base64:
 
     >>> n = 64 ** 5 + 1
-    >>> hhc_url_quote(base64.urlsafe_b64encode(long_to_binary(n)))
-    'QAAAAQ%3D%3D'
-    >>> hhc_url_quote(hhc(n))
-    'ucrDZ'
+    >>> print(hhc_url_quote(base64.urlsafe_b64encode(long_to_binary(n))))
+    QAAAAQ%3D%3D
+    >>> print(hhc(n))
+    ucrDZ
 
-Worst case for hexahexacontadecimal:
+Base64 vs HHC in a bad case for HHC:
 
     >>> n = 66 ** 5 + 1
-    >>> hhc_url_quote(base64.urlsafe_b64encode(long_to_binary(n)))
-    'SqUUIQ%3D%3D'
-    >>> hhc_url_quote(hhc(n))
-    '100001'
+    >>> print(hhc_url_quote(base64.urlsafe_b64encode(long_to_binary(n))))
+    SqUUIQ%3D%3D
+    >>> print(hhc(n))
+    100001
 
 That big SHA-512 you always wanted to write in a URL:
 
     >>> n = 2 ** 512
-    >>> hhc_url_quote(base64.urlsafe_b64encode(long_to_binary(n)))
-    'AQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA%3D'
-    >>> hhc_url_quote(hhc(n))
-    'JK84xqGD9FMXPNubPghADlRhBUzlqRscC2h~8xmi99PvuQsUCIB2CHGhMUQR8FLm72.Hbbctkqi89xspay~y4'
+    >>> print(hhc_url_quote(base64.urlsafe_b64encode(long_to_binary(n))))
+    AQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA%3D
+    >>> print(hhc(n))
+    JK84xqGD9FMXPNubPghADlRhBUzlqRscC2h~8xmi99PvuQsUCIB2CHGhMUQR8FLm72.Hbbctkqi89xspay~y4
 
-Massive savings.
+Massive savings!
 
 ### Are the savings really significant?
 
-If you're currently doing your Base64 encoding the naive way, then yes:
+If you're currently doing your Base64 encoding the naive way, then yes. Encoding all numbers up to 100000, HHC will lead
+to much shorter total length of URLs.
 
-    >>> sum(len(hhc_url_quote(base64.urlsafe_b64encode(long_to_binary(n)))) for n in xrange(10 ** 5))
+    >>> sum(len(hhc_url_quote(base64.urlsafe_b64encode(long_to_binary(n)))) for n in range(10 ** 5))
     531584
-    >>> sum(len(hhc_url_quote(hhc(n))) for n in xrange(10 ** 5))
+    >>> sum(len(hhc_url_quote(hhc(n))) for n in range(10 ** 5))
     295578
 
-### But what if I use Base64 without padding?
+#### What if I use Base64 without padding?
 
-Then the savings are not as significant. But it's still an improvement. Using the code from [this StackOverFlow
-question](http://stackoverflow.com/a/561704/76900):
+Then the savings are much less significant. Yet they are still savings. If you're a perfectionist this is the kind of
+thing you might care about.
+
+Let's test it using the code from [this StackOverFlow question](http://stackoverflow.com/a/561704/76900):
 
     >>> from hexahexacontadecimal.num_encode_base64 import num_encode as num_encode_base64
     >>> n = 64 ** 5 + 1
-    >>> hhc_url_quote(num_encode_base64(n))
-    'BAAAAB'
-    >>> hhc_url_quote(hhc(n))
-    'ucrDZ'
+    >>> print(hhc_url_quote(num_encode_base64(n)))
+    BAAAAB
+    >>> from hexahexacontadecimal.num_encode_base64 import num_decode as num_decode_base64
+    >>> num_decode_base64(hhc_url_quote(num_encode_base64(n))) == n
+    True
+
+    >>> print(hhc(n))
+    ucrDZ
+    >>> hhc_to_int(hhc(n)) == n
+    True
+
     >>> n = 66 ** 5 + 1
-    >>> hhc_url_quote(num_encode_base64(n))
-    'BKpRQh'
-    >>> hhc_url_quote(hhc(n))
-    '100001'
+    >>> print(hhc_url_quote(num_encode_base64(n)))
+    BKpRQh
+    >>> print(hhc_url_quote(hhc(n)))
+    100001
+
     >>> n = 2 ** 512
     >>> hhc_url_quote(num_encode_base64(n))
     'EAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
-    >>> hhc_url_quote(hhc(n))
-    'JK84xqGD9FMXPNubPghADlRhBUzlqRscC2h~8xmi99PvuQsUCIB2CHGhMUQR8FLm72.Hbbctkqi89xspay~y4'
-    >>> sum(len(hhc_url_quote(num_encode_base64(n))) for n in xrange(10 ** 5))
+    >>> print(hhc_url_quote(hhc(n)))
+    JK84xqGD9FMXPNubPghADlRhBUzlqRscC2h~8xmi99PvuQsUCIB2CHGhMUQR8FLm72.Hbbctkqi89xspay~y4
+
+    >>> sum(len(hhc_url_quote(num_encode_base64(n))) for n in range(10 ** 5))
     295840
-    >>> sum(len(hhc_url_quote(hhc(n))) for n in xrange(10 ** 5))
+    >>> sum(len(hhc_url_quote(hhc(n))) for n in range(10 ** 5))
     295578
 
 Why settle for less than perfect?
@@ -112,41 +126,52 @@ like -2, -1, 0, 1, 2.
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-from io import BytesIO
-import urllib
+import codecs
+from io import StringIO
+
+try:
+    from urllib.parse import quote
+except ImportError:
+    from urllib import quote
 
 __all__ = ['hhc', 'hhc_to_int', 'hhc_url_quote', 'sortable_hhc', 'sortable_hhc_to_int']
 
-NEGATIVE_PREFIX = b","
-BASE66_ALPHABET = b"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_.~"
-SORTABLE_BASE66_ALPHABET = b"-.0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz~"
+NEGATIVE_PREFIX = ","
+BASE66_ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-_.~"
+SORTABLE_BASE66_ALPHABET = "-.0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz~"
 BASE = len(BASE66_ALPHABET)
 
 
 def hhc_url_quote(s, safe=None):
-    """Like urllib.quote() but don't escape ~, in accordance with RFC3986.
+    """Like urllib.parse.quote() but don't escape ~, in accordance with RFC3986.
 
-    >>> hhc_url_quote(hhc(65))
-    '~'
+    >>> print(hhc_url_quote(hhc(65)))
+    ~
     """
 
-    return urllib.quote(s, safe='~' + (safe or ''))
+    return quote(s, safe='~' + (safe or ''))
 
 
 def long_to_binary(n):
     """Take an integer and write it as a binary string.
 
-    >>> long_to_binary(0)
-    '\\x00'
-    >>> long_to_binary(255)
-    '\\xff'
-    >>> long_to_binary(512 + 3)
-    '\\x02\\x03'
+    >>> long_to_binary(0) == b'\\x00'
+    True
+    >>> long_to_binary(255) == b'\\xff'
+    True
+    >>> long_to_binary(512 + 3) == b'\\x02\\x03'
+    True
     """
 
-    h = '%x' % n
-    return ('0' * (len(h) % 2) + h).decode('hex')
+    if hasattr(n, 'to_bytes'):
+        if n == 0:
+            return b'\x00'
 
+        return n.to_bytes((n.bit_length()+7)//8, 'big')
+    else:
+        # Python 2
+        h = '%x' % n
+        return ('0'*(len(h) % 2) + h).decode('hex')
 
 def binary_to_long(b):
     """Take a binary string and read it as an integer.
@@ -159,28 +184,28 @@ def binary_to_long(b):
     515
     """
 
-    return int(b.encode('hex'), 16)
+    return int(codecs.encode(b, 'hex'), 16)
 
 
 def hhc(n, alphabet=BASE66_ALPHABET):
     """Represent a number in hexahexacontadecimal, a compact ASCII format of unreserved URL characters.
 
-    >>> hhc(0)
-    '0'
-    >>> hhc(1)
-    '1'
-    >>> hhc(65)
-    '~'
-    >>> hhc(66)
-    '10'
-    >>> hhc(67)
-    '11'
-    >>> hhc(302231454903657293676544)
-    'iFsGUkO.0tsxw'
+    >>> print(hhc(0))
+    0
+    >>> print(hhc(1))
+    1
+    >>> print(hhc(65))
+    ~
+    >>> print(hhc(66))
+    10
+    >>> print(hhc(67))
+    11
+    >>> print(hhc(302231454903657293676544))
+    iFsGUkO.0tsxw
 
     Negative numbers are not URL safe and should be avoided:
-    >>> hhc(-67)
-    ',11'
+    >>> print(hhc(-67))
+    ,11
 
     """
 
@@ -190,7 +215,7 @@ def hhc(n, alphabet=BASE66_ALPHABET):
     if n < 0:
         return NEGATIVE_PREFIX + hhc(-n, alphabet=alphabet)
 
-    r = BytesIO()
+    r = StringIO()
     while n:
         n, t = divmod(n, BASE)
         r.write(alphabet[t])
@@ -211,8 +236,8 @@ def hhc_to_int(s, alphabet=BASE66_ALPHABET):
     66
     >>> hhc_to_int('11')
     67
-    >>> hhc_to_int('iFsGUkO.0tsxw')
-    302231454903657293676544L
+    >>> print(hhc_to_int('iFsGUkO.0tsxw'))
+    302231454903657293676544
     >>> hhc_to_int(',11')
     -67
 
@@ -242,20 +267,20 @@ def sortable_hhc(n, width=0, alphabet=SORTABLE_BASE66_ALPHABET):
     goes "0", "1", "2", ... "10", "11", "12"... which looks nice and natural. With sortable_hhc the same series is
     "-", ".", ".-", ".." which looks like some kind of morse code.
 
-    >>> sortable_hhc(0)
-    '-'
-    >>> sortable_hhc(1)
-    '.'
-    >>> sortable_hhc(65)
-    '~'
-    >>> sortable_hhc(66)
-    '.-'
-    >>> sortable_hhc(67)
-    '..'
-    >>> sortable_hhc(6700)
-    '.XW'
-    >>> sortable_hhc(302231454903657293676544)
-    'fDpEShMz-qput'
+    >>> print(sortable_hhc(0))
+    -
+    >>> print(sortable_hhc(1))
+    .
+    >>> print(sortable_hhc(65))
+    ~
+    >>> print(sortable_hhc(66))
+    .-
+    >>> print(sortable_hhc(67))
+    ..
+    >>> print(sortable_hhc(6700))
+    .XW
+    >>> print(sortable_hhc(302231454903657293676544))
+    fDpEShMz-qput
 
     With standard HHC, alphabetical comparison is not the same as numeric comparison:
     >>> hhc(67) < hhc(128)
@@ -271,18 +296,18 @@ def sortable_hhc(n, width=0, alphabet=SORTABLE_BASE66_ALPHABET):
     The width parameter allows you to set a minimum width. As long as that width is wider than you'll ever need,
     all generated values will sort alphabetically without decoding.
 
-    >>> sortable_hhc(67, width=4)
-    '--..'
+    >>> print(sortable_hhc(67, width=4))
+    --..
     >>> sorted([sortable_hhc(x, width=2) for x in range(512)]) == [sortable_hhc(x, width=2) for x in range(512)]
     True
 
     Negative numbers are supported and will work like you expect, sorting like -2, -1, 0, 1, 2. (Note that the alphabet
     is reversed for negative numbers.)
 
-    >>> sortable_hhc(-67)
-    ',zz'
-    >>> sortable_hhc(-6700)
-    ',zST'
+    >>> print(sortable_hhc(-67))
+    ,zz
+    >>> print(sortable_hhc(-6700))
+    ,zST
     >>> sortable_hhc(-50, width=5) < sortable_hhc(-1, width=5)
     True
     >>> sortable_hhc(-1, width=5) < sortable_hhc(0, width=5)
@@ -320,8 +345,8 @@ def sortable_hhc_to_int(s):
     6700
     >>> sortable_hhc_to_int('----..')
     67
-    >>> sortable_hhc_to_int('fDpEShMz-qput')
-    302231454903657293676544L
+    >>> print(sortable_hhc_to_int('fDpEShMz-qput'))
+    302231454903657293676544
 
     Negative numbers are supported.
 
